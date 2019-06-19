c3ec21972832f0734a1879f44e646440c6c0f3d0
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-06-19 10:21:21 -0500

    actualizado

---
+++ ejercicio.js
@@ -0,0 +1,36 @@
+var totalPuntos = 1230;
+var amigos = ["Mike","Lucas","Dustin","Eleven","Max"];
+var artefactos = 3;
+var puntosArtefactosPorAmigo = [0,0,0,0,0]
+var sumaTotal = 0;
+var porcentaje = 0;
+for(var i = 0; i<amigos.length;i++){
+    for(var j = 0; j < artefactos; j++){
+        do{
+            var artefacto = prompt("Digite los puntos"+
+            " del artefacto "+(j+1)+" recolectado por "+
+            amigos[i]);
+            artefacto = parseInt(artefacto)
+        }while(artefacto < 1 || artefacto > 100)
+        
+        puntosArtefactosPorAmigo[i] += artefacto;
+
+        //alert("el total de puntos va en "+
+        //puntosArtefactosPorAmigo[i]);
+    }
+}
+
+for(var k = 0; k < puntosArtefactosPorAmigo.length; k++){
+    document.write("<h3>Los puntos que recogió "+
+    amigos[k]+" fueron en total "
+    + puntosArtefactosPorAmigo[k])
+}
+
+for(var i=0; i<puntosArtefactosPorAmigo.length;i++){
+           sumaTotal += puntosArtefactosPorAmigo[i];
+}
+
+           porcentaje=sumaTotal*100 / totalPuntos;
+           
+            
+          document.write("<h3>El porcentaje del total de puntos es: " + porcentaje + "%");
